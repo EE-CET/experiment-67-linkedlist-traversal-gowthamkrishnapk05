d4375f1d2787464a24adc2d0c5613dbe6981cf3b
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-03-06 09:32:36 +0530

    updated

--- src/Solution.java
+++ src/Solution.java
@@ -2,36 +2,44 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
 
-public class LinkedListTraversal {
+public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read the number of elements
+        // Read N (the number of elements)
         if (!sc.hasNextInt()) return;
         int n = sc.nextInt();
 
         LinkedList<Integer> list = new LinkedList<>();
 
-        // Add elements to the LinkedList
+        // Add N integers to the list
         for (int i = 0; i < n; i++) {
-            list.add(sc.nextInt());
+            if (sc.hasNextInt()) {
+                list.add(sc.nextInt());
+            }
         }
 
-        // Obtain the ListIterator
+        // Initialize the ListIterator
         ListIterator<Integer> it = list.listIterator();
 
         // 1. Forward Traversal
         System.out.print("Forward: ");
         while (it.hasNext()) {
-            System.out.print(it.next() + (it.hasNext() ? " " : ""));
+            System.out.print(it.next());
+            if (it.hasNext()) {
+                System.out.print(" ");
+            }
         }
         System.out.println();
 
         // 2. Backward Traversal
-        // Note: After the forward loop, the cursor 'it' is at the end of the list.
+        // The iterator 'it' is currently at the end of the list
         System.out.print("Backward: ");
         while (it.hasPrevious()) {
-            System.out.print(it.previous() + (it.hasPrevious() ? " " : ""));
+            System.out.print(it.previous());
+            if (it.hasPrevious()) {
+                System.out.print(" ");
+            }
         }
         System.out.println();
 
